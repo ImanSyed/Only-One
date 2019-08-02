78c2101a7f9331646e9ef082944752e153681c23
ImanSyed <31649603+ImanSyed@users.noreply.github.com>
2019-08-03 02:16:58 +0530

    First commit
    Basic stuff

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    [SerializeField]
+    float snapValue = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = new Vector3(Round(mousePos.x), Round(mousePos.y), -9);
+    }
+
+    private float Round(float input)
+    {
+        return (snapValue * Mathf.Round(input / snapValue) + 0.5f);
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
