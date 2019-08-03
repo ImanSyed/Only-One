02d4ab2ed05eafb899556949fb4ce55e550a7928
Abu Bakarr Bangurah <animevideogamer@gmail.com>
2019-08-03 03:02:27 -0400

    8/3/2019
    Update with working turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] Transform targetPlayer;
+    [SerializeField] Transform objectToPan;
+    public GameObject [] missle;
+    public GameObject missleEmiiter;
+    [SerializeField]  float misslespeed = -15.0f;
+    [SerializeField] float timeDestroyed = 1.0f;
+    [SerializeField] float bulletInterval = 1.0f;
+    GameObject TempBullet;
+    Rigidbody2D TempRigid;
+
+
+
+    // Update is called once per frame
+    void Start()
+    {
+       StartCoroutine(ShootProjectille());
+    }
+
+     IEnumerator ShootProjectille()
+    {
+        while (true)
+        {
+            TempBullet = Instantiate(missle[UnityEngine.Random.Range(0, missle.Length)], missleEmiiter.transform.position, missleEmiiter.transform.rotation);
+            TempRigid = TempBullet.GetComponent<Rigidbody2D>();
+            TempRigid.AddForce(Vector2.right * misslespeed);
+            yield return new WaitForSeconds(bulletInterval);
+            Destroy(TempBullet, timeDestroyed);
+        }
+
+    }
+}
