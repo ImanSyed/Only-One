5cccd73cced23d601857df83fe7e8c8991461046
ImanSyed <31649603+ImanSyed@users.noreply.github.com>
2019-08-03 10:24:01 +0530

    Size manipulation

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -5,6 +5,8 @@ public class ItemScript : MonoBehaviour
     [SerializeField]
     float snapValue = 1;
 
+    bool axis;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +20,50 @@ public class ItemScript : MonoBehaviour
         mousePos.z = 0;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
         transform.position = new Vector3(Round(mousePos.x), Round(mousePos.y));
+
+        Manipulation();
     }
 
     private float Round(float input)
     {
         return (snapValue * Mathf.Round(input / snapValue) + 0.5f);
+    }
+
+    private void Manipulation()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            axis = !axis;
+        }
+        if (!axis)
+        {
+            if (Input.GetAxisRaw("Size") > 0 && transform.localScale.x < 4f)
+            {
+                Vector3 scale = transform.localScale;
+                scale.x += 0.25f;
+                transform.localScale = scale;
+            }
+            else if (Input.GetAxisRaw("Size") < 0 && transform.localScale.x > 0.5f)
+            {
+                Vector3 scale = transform.localScale;
+                scale.x -= 0.25f;
+                transform.localScale = scale;
+            }
+        }
+        else
+        {
+            if (Input.GetAxisRaw("Size") > 0 && transform.localScale.y < 4f)
+            {
+                Vector3 scale = transform.localScale;
+                scale.y += 0.25f;
+                transform.localScale = scale;
+            }
+            else if (Input.GetAxisRaw("Size") < 0 && transform.localScale.y > 0.5f)
+            {
+                Vector3 scale = transform.localScale;
+                scale.y -= 0.25f;
+                transform.localScale = scale;
+            }
+        }
     }
 }
